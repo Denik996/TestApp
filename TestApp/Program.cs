ea03123ea7e21c2253bf76060eca81eaed9e0ac8
Denys Shtelikha <denik996@gmail.com>
2016-10-31 22:19:20 +0300

    Support downloading (stop, resuming, continue)

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -10,6 +10,31 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
+            IFileDownloader fileDownloader = new FileDownloader();
+            fileDownloader.DownloadProgressChanged += OnDownloadProgressChanged;
+            fileDownloader.DownloadFileCompleted += DownloadFileCompleted;
+            fileDownloader.DownloadFileAsync(new Uri("http://kubix-service.info/download"), "D:\\TestRar.rar");
+            Console.ReadLine();
+        }
+
+
+        static void DownloadFileCompleted(object sender, DownloadFileCompletedArgs eventArgs)
+        {
+            if (eventArgs.State == CompletedState.Succeeded)
+            {
+                //download completed
+            }
+            else if (eventArgs.State == CompletedState.Failed)
+            {
+                //download failed
+            }
+        }
+
+        static void OnDownloadProgressChanged(object sender, DownloadFileProgressChangedArgs args)
+        {
+            Console.Clear();
+            Console.WriteLine(args.ProgressPercentage.ToString() + "%");
+            //Console.WriteLine("Download {0} bytes from {1} bytes", args.BytesReceived, args.TotalBytesToReceive);
         }
     }
 }
---
+++ TestApp/Search.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TestApp
+{
+    public class Search
+    {
+        string _filename;
+        long _timeout;
+        List<Thread> _threads;
+        List<string> _foundResult;
+        System.Timers.Timer _timer;
+        bool _isSearching = false;
+
+
+        public Search(string filename)
+        {
+            _filename = filename;
+        }
+
+        public void StartSearch(long timeout = 0)
+        {
+            _timeout = timeout;
+
+            if (timeout != 0)
+            {
+                _timer = new System.Timers.Timer();
+                _timer.Interval = _timeout;
+                _timer.Elapsed += new System.Timers.ElapsedEventHandler(_timer_Elapsed);
+                _timer.Start();
+            }
+
+            _isSearching = true;
+
+            _threads = new List<Thread>();
+            _foundResult = new List<string>();
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (var drive in drives)
+            {
+                Thread findThread = new Thread(new ParameterizedThreadStart(FindOnDrive));
+                findThread.Name = "Thread " + drive.Name.Substring(0, 1);
+                _threads.Add(findThread);
+            }
+
+            for (int i = 0; i < _threads.Count; i++)
+            {
+                _threads[i].Start(drives[i].Name);
+            }
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _isSearching = false;
+            _timer.Stop();
+        }
+
+        private void FindOnDrive(object driveName)
+        {
+            if (_isSearching)
+            {
+                string drive = (string)driveName;
+                try
+                {
+                    foreach (string d in Directory.GetDirectories(drive))
+                    {
+                        foreach (string f in Directory.GetFiles(d, _filename))
+                        {
+                            _foundResult.Add(f);
+                            Console.WriteLine(f);
+                            StopSearch();
+                        }
+                        FindOnDrive(d);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                foreach (var thread in _threads)
+                {
+                    thread.Abort();
+                    thread.Join();
+                }
+            }
+        }
+
+        private void StopSearch()
+        {
+            _isSearching = false;
+            _timer.Stop();
+        }
+    }
+}
